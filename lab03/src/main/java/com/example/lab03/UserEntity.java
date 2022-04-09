ac980300e7a871e75afcd994b08a10b7796d0fba
Mateusz Pocztowski <mateusz.pocztowski@microsoft.wsei.edu.pl>
2022-04-09 16:55:16 +0200

    Created lab03 project

---
+++ lab03/src/main/java/com/example/lab03/UserEntity.java
@@ -0,0 +1,38 @@
+package com.example.lab03;
+
+public class UserEntity
+{
+    private int id;
+    private int age;
+    private String name;
+
+    public UserEntity(int id, String name, int age) {
+        this.id = id;
+        this.age = age;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ lab03/src/main/java/com/example/lab03/UsersController.java
@@ -0,0 +1,50 @@
+package com.example.lab03;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+public class UsersController {
+    private Map<Integer, UserEntity> users = new HashMap<Integer,UserEntity>();
+
+    @RequestMapping("/users")
+    @ResponseBody
+    public Object getAllUsers() {
+        return this.users;
+    }
+
+    @RequestMapping("/users/{id}/get")
+    @ResponseBody
+    public Object getUser(@PathVariable Integer id) {
+        return this.users.get(id);
+    }
+
+    @RequestMapping("/users/{id}/remove")
+    @ResponseBody
+    public Object removeUser(@PathVariable Integer id) {
+        return this.users.remove(id);
+    }
+
+    @RequestMapping("/users/add")
+    @ResponseBody
+    public Object addUser(
+            @RequestParam(required = true) String name,
+            @RequestParam(required = true) Integer age
+    ) {
+        int id = 0;
+        while (this.users.containsKey(id)) {
+            id++;
+        }
+
+        UserEntity user = new UserEntity(id, name, age);
+        this.users.put(id, user);
+
+        return this.users.get(id);
+    }
+}
