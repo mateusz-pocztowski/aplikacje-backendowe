3d79d01d8fdb5b572db236a53a98118e6bc276a2
Mateusz Pocztowski <mateusz.pocztowski@summer.agency>
2022-06-16 18:13:12 +0200

    Created gateway interceptor to catch miroservice's error responses

---
+++ projekt/backend/api-gateway/src/app.interceptor.ts
@@ -0,0 +1,24 @@
+import {
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+  HttpException,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Injectable()
+export class AppInterceptor implements NestInterceptor {
+  intercept(_context: ExecutionContext, next: CallHandler): Observable<any> {
+    return next.handle().pipe(
+      map((data) => {
+        if (data.error) {
+          throw new HttpException(data, data.status);
+        }
+
+        return data;
+      }),
+    );
+  }
+}
--- projekt/backend/api-gateway/src/app.module.ts
+++ projekt/backend/api-gateway/src/app.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { AppInterceptor } from './app.interceptor';
 import { AuthModule } from './auth/auth.module';
 import { MoviesModule } from './movies/movies.module';
 
 @Module({
   imports: [AuthModule, MoviesModule],
+  providers: [{ provide: APP_INTERCEPTOR, useClass: AppInterceptor }],
 })
 export class AppModule {}
